e2f934f56fb64ca56c49d704643066b51459297b
MINJUN JUNG <93313445+BanApp@users.noreply.github.com>
2022-09-21 18:17:10 +0900

    Create 0921-2.c

---
+++ 0921-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 1;
+    scanf("%d",&x);
+    int cnt = 0;
+    for(int i=1; i<=x; i++){
+        if(x%i == 0){
+            cnt += 1;
+            printf("%d ", i);
+        }
+    }
+    if(cnt==2){
+        printf("\n");
+        printf("소수");
+        }
+
+    return 0;
+}
