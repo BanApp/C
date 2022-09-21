84b893db00786e92493804179e38e29105601528
minjunjung <jmj284@gmail.com>
2022-09-21 17:42:57 +0900

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i=1; i<101; i++)
+        {
+            if (i%5 == 0)
+            {
+                printf("%d",i);
+                printf("\n");
+            }
+
+            else
+            {
+                printf("%d ",i);
+            }
+        }
+    return 0;
+}
